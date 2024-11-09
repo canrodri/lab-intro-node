16433a2efc2d3ce58e6f2cb24a22bff9d22dc381
candela <candelap.r.p@gmail.com>
2024-11-09 11:16:09 +0100

    done

--- index.js
+++ index.js
@@ -1,17 +1,57 @@
 class SortedList {
-  constructor() {}
+  constructor() {
+    this.items = [];
+    this.length = 0;
+  }
 
-  add(item) {}
+  add(item) { 
+    this.items.push(item);
+    this.items.sort((a,b) => a - b);
+    this.length = this.items.length;
+  }
 
-  get(pos) {}
+  get(pos) {
+    const valueSearch = this.items[pos];
+    if (!valueSearch) {
+      throw new Error ("OutOfBounds");
+    }
+    return valueSearch;    
+  }
+  max() { 
+    const maxValue = Math.max(...this.items);
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return maxValue;
 
-  max() {}
+  }
 
-  min() {}
+  min() { 
+    const minValue = Math.min(...this.items); 
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+    return minValue
+  }
 
-  sum() {}
+  sum() {
 
-  avg() {}
+    let sumItems = 0
+    for(let i = 0; i < this.items.length; i++) {
+      const item = this.items[i]
+      sumItems += item
+    }
+  return sumItems
+  }
+
+  avg() {
+    if(this.items.length === 0) {
+      throw new Error("EmptySortedList");
+    }
+        const averageItem = this.sum() / this.items.length;
+        return averageItem;
+   }
+   
 }
 
 module.exports = SortedList;
